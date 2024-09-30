a145ce06ecba0ddeb630e7e56885b7d1f1b34643
annailvyadne <annailvyadne@gmail.com>
2024-09-30 15:04:30 +0800

    Updated

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -7,35 +7,5 @@ import emailjs from 'emailjs-com';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  name: string = '';
-  email: string = '';
-  message: string = '';
-  successMessage: boolean = false; // Variable to track success message
-
-  sendEmail(e: Event) {
-    e.preventDefault(); // Prevent default form submission
-
-    const templateParams = {
-      from_name: this.name,
-      from_email: this.email,
-      reply_to: this.email,  // Use the user's email address for replies
-      message: this.message,
-    };
-
-    emailjs.send('service_nvy8xop', 'template_wmv3skt', templateParams, '4PkGe2pSHP3wYMzoG')
-    .then((response) => {
-      console.log('Email sent successfully!', response.status, response.text);
-      // Resetting form fields
-      this.name = '';
-      this.email = '';
-      this.message = '';
-
-// Hide the success message after 5 seconds
-      setTimeout(() => {
-        this.successMessage = false;
-      }, 5000);
-    }, (err) => {
-      console.error('Failed to send email. Error: ', err);
-    });
-  }
+ 
 }
